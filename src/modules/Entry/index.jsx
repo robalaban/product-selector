b07a563b7651846d10f8e6b9683ad2fd86253af8
Robert Balaban <robert@myhost.local>
2018-04-22 13:36:35 +0300

    Bootstrapped Entry page with Product List

--- src/modules/Entry/index.jsx
+++ src/modules/Entry/index.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react"
 import Data from "../../data/sample_data"
 
+const SingleProduct = ({ ...props }) => (
+  <li className="products--single">
+    <img
+      className="products--single__image"
+      src={props.image}
+      alt={props.name}
+    />
+    <h2 className="products--single__name">{props.name}</h2>
+  </li>
+)
+
 class App extends Component {
   render() {
+    const data = Data.products
+    console.log(data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">React Product Selector</h1>
-        </header>
+      <div className="container">
+        <h1 className="container--title">Our Products</h1>
+        <ul className="products">
+          {data.map((val, idx) => (
+            <SingleProduct image={val["image"]} name={val["name"]} key={idx} />
+          ))}
+        </ul>
       </div>
     )
   }
